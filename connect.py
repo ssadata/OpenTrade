512dcc9bd6b7883cca8c17d837761644fe9f0af3
ssadata <60407190+ssadata@users.noreply.github.com>
2020-11-30 16:06:46 -0300

    Update connect.py

--- connect.py
+++ connect.py
@@ -7,6 +7,9 @@ tables = json.loads(requests.get("https://api.tradestatistics.io/tables").text)
 pd.DataFrame.from_dict(tables, orient='columns')
 
 # bilateral and product detailed data
-trade = json.loads(
-	requests.get("https://api.tradestatistics.io/yrpc?y=2018&r=chl&p=arg").text)
+trade = json.loads(requests.get("https://api.tradestatistics.io/yrpc?y=2018&r=chl&p=arg").text)
 pd.DataFrame.from_dict(trade, orient='columns')
+
+# detailed products 
+products = json.loads(requests.get("https://api.tradestatistics.io/products").text)
+pd.DataFrame.from_dict(products, orient='columns')
